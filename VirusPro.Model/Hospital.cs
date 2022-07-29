2e7ab4212dc7df0f034d35f3664c05835f33f556
Eleanora <Eleanora@qq.com>
2022-07-29 17:30:01 +0800

    使用WTM框架添加了Virus类，初始化项目

---
+++ VirusPro.Model/Hospital.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusPro.Model
+{
+    public enum HospitalLevelEnum
+    {
+        Class1,
+        Class2,
+        Class3
+    }
+    class Hospital
+    {
+        [Display(Name = "医院名称")]
+        [Required(ErrorMessage = "医院名称是必填项")]
+        public string HostpitalName { get; set; }
+        [Display(Name = "医院级别")]
+        [Required(ErrorMessage = "医院级别是必填项")]
+        public HospitalLevelEnum HostpitalLevel { get; set; }
+        /*
+         * Hostpital表中的属性Location为外键，参考自City表中CityName(一个city有多个hospital)
+         */
+        public City Location { get; set; }
+        public Guid LocationId { get; set; }
+
+        
+    }
+}
+
---
+++ VirusPro.Model/Virus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalkingTec.Mvvm.Core;
+
+namespace VirusPro.Model
+{
+    public enum VirusTypeEnum
+    {
+        RNA, DNA
+    }
+    public class Virus : TopBasePoco
+    {
+        [Display(Name = "病毒名称")]
+        [Required(ErrorMessage = "病毒名称是必填项")]
+        public string VirusName { get; set; }
+
+        [Display(Name = "病毒代码")]
+        [Required(ErrorMessage = "病毒代码是必填项")]
+        [StringLength(10, ErrorMessage = "病毒代码最多10个字符")]
+        public string VirusCode { get; set; }
+
+        [Display(Name = "病毒描述")]
+        public string VirusRemark { get; set; }
+
+        [Display(Name = "病毒种类")]
+        [Required(ErrorMessage = "病毒种类是必填项")]
+        public VirusTypeEnum VirusType { get; set; }
+
+    }
+}
